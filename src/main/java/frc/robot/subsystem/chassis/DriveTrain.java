fe8bb0e1d218cc0ebadeea6d20f358f1555b474a
“yoavkaplan123git config --global user.email “yoav110702@gmail.com <yoav110702@gmail.com>
2023-12-13 16:52:26 +0200

    first commit

---
+++ src/main/java/frc/robot/subsystem/chassis/DriveTrain.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystem.chassis;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+  /** Creates a new driveTrain. */
+  private CANSparkMax leftMotor;
+  private CANSparkMax rightMotor;
+
+  private static DriveTrain instance;
+
+  private DriveTrain() {
+    leftMotor = new CANSparkMax(2, MotorType.kBrushless);
+    rightMotor = new CANSparkMax(3, MotorType.kBrushless);
+  }
+
+  public void setLeft(double power) {
+    leftMotor.set(power);
+  }
+
+  public void setRight(double power) {
+    rightMotor.set(power);
+  }
+
+  public static DriveTrain getInstance() {
+    if (instance == null) {
+      instance = new DriveTrain();
+    }
+    return instance;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
